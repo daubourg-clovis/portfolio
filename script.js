598d360ebc43010a762103020827672597b6b67b
daubourg-clovis <daubourg.clovis@gmail.com>
2020-07-21 13:55:39 +0200

    Update - style header

---
+++ script.js
@@ -0,0 +1,34 @@
+// function myFunction(){
+//     document.getElementById('myDropdown').classList.toggle('show');
+// }
+
+// window.onclick = function(event){
+//     if (!event.target.matches('.btn-drop')){
+//         let dropdowns = document.getElementsByClassName('dropdown-content');
+//         let i;
+//         for(i = 0; i < dropdowns.length; i++){
+//             let openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')){
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.btn-drop')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
