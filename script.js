c565f7045f476c50301d0be34ecc33b6439c1fe9
daubourg-clovis <daubourg.clovis@gmail.com>
2020-07-21 14:17:55 +0200

    Update - style header end

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-// function myFunction(){
-//     document.getElementById('myDropdown').classList.toggle('show');
-// }
 
-// window.onclick = function(event){
-//     if (!event.target.matches('.btn-drop')){
-//         let dropdowns = document.getElementsByClassName('dropdown-content');
-//         let i;
-//         for(i = 0; i < dropdowns.length; i++){
-//             let openDropdown = dropdowns[i];
-//             if (openDropdown.classList.contains('show')){
-//                 openDropdown.classList.remove('show');
-//             }
-//         }
-//     }
-// }
 
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
