df27c7e60ce50e19a48f3a51af1b7d9e675a1c38
Clovis Daubourg <62110612+daubourg-clovis@users.noreply.github.com>
2020-07-23 16:32:24 +0200

    Update - end od styling

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+//   }
   
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.btn-drop')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+//   // Close the dropdown if the user clicks outside of it
+//   window.onclick = function(event) {
+//     if (!event.target.matches('.btn-drop')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
+
+function showLanguages() {
+  $(".dropdown-language").toggleClass("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.lang-drpdwn-btn')) {
+    var dropdowns = $(".dropdown-language");
+    for (let i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
